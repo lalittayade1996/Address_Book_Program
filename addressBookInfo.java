49d8f67d231a78245afc75695dd1aeb913378254
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-06-22 01:41:06 +0530

    Added : Address Book Info

---
+++ addressBookInfo.java
@@ -0,0 +1,24 @@
+
+class CreatContacts{
+	String fName, lName, address, city, state;
+   Long zipCode, phoneNum;
+   String email;
+
+		CreatContacts(String fName, String lName, String address, String city, String state, long zipCode, long phoneNum, String email){ 
+			this.fName = fName;
+			this.lName = lName;
+			this.address = address;
+			this.city = city;
+			this.state = state;
+			this.zipCode = zipCode;
+			this.phoneNum = phoneNum;
+			this.email = email;
+		} 
+}
+
+
+public class addressBookInfo {
+public static void main(String[] args) {
+System.out.println("WELCOME TO ADDRESS BOOK PROGRAMS");
+}
+}
